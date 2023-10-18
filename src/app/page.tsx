8d3f3ac3bc8f72db3b6d525c3c7f20f08cb4d49b
Rogério Teixeira Vidal <103948082+RogerioTVidal@users.noreply.github.com>
2023-10-18 10:14:24 -0300

    feat: add sheet structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,43 @@
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
-import { MenuIcon, ShoppingCartIcon } from 'lucide-react'
+import { Sheet, SheetContent, SheetHeader, SheetTrigger,} from "@/components/ui/sheet"
+import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentIcon, ShoppingCartIcon } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
   return <Card className='flex items-center justify-between p-7'>
-    <Button size='icon' variant='outline'>
-      <MenuIcon />
-    </Button>
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size='icon' variant='outline'>
+          <MenuIcon />
+        </Button>
+      </SheetTrigger>
 
+      <SheetContent side={'left'}>
+        <SheetHeader className='text-left text-lg font-semibold'>Menu</SheetHeader>
+        <div className="mt-2 flex flex-col gap-2"> 
+        <Button variant={'outline'} className='w-full justify-start gap-2'>
+            <LogInIcon size={16} />
+            Login
+          </Button>
+
+          <Button variant={'outline'} className='w-full justify-start gap-2'>
+            <HomeIcon size={16} />
+            Home
+          </Button>
+
+          <Button variant={'outline'} className='w-full justify-start gap-2'>
+            <PercentIcon size={16} />
+            Offers
+          </Button>
+
+          <Button variant={'outline'} className='w-full justify-start gap-2'>
+            <ListOrderedIcon size={16} />
+            Catalog
+          </Button>
+        </div>
+      </SheetContent>
+    </Sheet>
     <h1 className='text-lg font-semibold'>
       <span className='text-primary'>IT</span> Store
     </h1>
